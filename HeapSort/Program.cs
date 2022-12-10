7b69db366095cbef079845df7f9bba4f432a9b73
HanaDanis <hanadanis3@gmail.com>
2022-12-10 19:41:53 +0200

    adding files

---
+++ HeapSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HeapSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            Sort ob = new Sort();
+            ob.sort(arr);
+            Console.WriteLine("Sorted array is");
+            Console.WriteLine(String.Join(" ", arr));
+
+            //Explanation:
+            //Convert from array to list = o(n)
+            //Heap sort = o(log(n))
+            //Total = o((n)log(n))
+        }
+    }
+}
